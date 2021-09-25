a4a679f574d9ec707f24308cece0340ba2ce805f
root <root@knazarov.com>
2021-09-25 21:00:14 +0000

    Add UTF-8 encoding

--- bin/page.sh
+++ bin/page.sh
@@ -6,6 +6,7 @@ expand() {
 <html lang="en">
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="/style.css">
     <title>
 	EOF
