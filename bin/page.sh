5b67f53fc6946e99969842f0253f5ebcddbdeede
Konstantin Nazarov <mail@knazarov.com>
2025-01-13 19:30:34 +0000

    Add google tag manager

--- bin/page.sh
+++ bin/page.sh
@@ -5,29 +5,41 @@ expand() {
 	cat <<-"EOF"
 <html lang="en">
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="/style.css">
-    <title>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WKVVNZ8F');</script>
+<!-- End Google Tag Manager -->
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="/style.css">
+	<title>
 	EOF
 
 	echo "      $TITLE"
 
 	cat <<-"EOF"
-    </title>
-	<link rel="alternate" type="application/rss+xml" 
+	</title>
+	<link rel="alternate" type="application/rss+xml"
 		title="RSS Feed for vmatveeva.com" href="/rss.xml" />
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" /> 
+	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
   </head>
   <body>
-    <div id="header">
-      <a href="/">Valeriya Matveeva</a>
-    </div>
-    <div id="menu">
-        <a href="/">home</a>
-        <a href="/contact">contact</a>
-    </div>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKVVNZ8F"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+	<div id="header">
+	  <a href="/">Valeriya Matveeva</a>
+	</div>
+	<div id="menu">
+		<a href="/">home</a>
+		<a href="/contact">contact</a>
+	</div>
 	<div id="content">
 EOF
 	cat
